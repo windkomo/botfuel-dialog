d71c4a5b9a7bbd180f6f0f743231f5bb67ef950d
Sattisvar <sattisvar@botfuel.io>
2017-10-13 14:35:50 +0200

    expose FileCorpus

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const Classifier = require('./classifier');
 const CorpusExtractor = require('./extractors/corpus_extractor');
 const Dialog = require('./dialogs/dialog');
 const DialogManager = require('./dialog_manager');
+const FileCorpus = require('./corpora/file_corpus');
 const MemoryBrain = require('./brains/memory_brain');
 const MongoBrain = require('./brains/mongo_brain');
 const Messages = require('./messages');
@@ -17,6 +18,7 @@ module.exports = {
   CorpusExtractor,
   Dialog,
   DialogManager,
+  FileCorpus,
   MemoryBrain,
   MongoBrain,
   Messages,
